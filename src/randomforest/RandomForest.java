b1cd9cfb6bf1c991515185f816b810809b2901cb
p-przybylek <pp.paulinaprzybylek@gmail.com>
2020-01-25 19:07:08 +0100

    Wydmuszka las

--- src/randomforest/RandomForest.java
+++ src/randomforest/RandomForest.java
@@ -1,4 +1,4 @@
-package RandomForest;
+package randomforest;
 
 import dataload.DataFrame;
 import decisiontree.DecisionTree;
@@ -8,13 +8,34 @@ import java.util.ArrayList;
 public class RandomForest {
     int height;
     DataFrame dataFrame;
-    double rowPrecentage;
+    double rowPercentage;
     double columnPercentage;
+    double DivisionPercentage;
 
-    public RandomForest(int height, DataFrame dataFrame, double rowPrecentage, double columnPercentage) {
+    public RandomForest(int height, DataFrame dataFrame, double rowPercentage, double columnPercentage, double DivisionPercentage) {
         this.height = height;
         this.dataFrame = dataFrame;
-        this.rowPrecentage = rowPrecentage;
+        this.rowPercentage = rowPercentage;
         this.columnPercentage = columnPercentage;
+        this.DivisionPercentage = DivisionPercentage;
     }
+
+    //chcemy gdzies sprawdzac co przewiduje - czy ma ustawione - moze w train()
+
+    private ArrayList<DataFrame> DivisionData(){
+
+        //korzystamy z DiviosionPercentage i dzielimy DataFrame
+
+    }
+
+    public ArrayList<DecisionTree> train(){
+
+        //chcemy wybierac kolumny oprocz tej do przewidywania - losowo
+
+    }
+
+    public double test(){
+
+    }
+
 }
